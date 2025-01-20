be9b91bbb35e931e0a45eb5894a073997b06a0ad
kokosheidelbeere <mastjura@gmail.com>
2025-01-20 23:27:53 +0100

    added api key

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,6 +1,6 @@
 const map = new maplibregl.Map({
     container: 'map',
-    style: 'https://static.maptoolkit.net/rapidapi/styles/terrain.json?rapidapi-key=YOUR_API_KEY', // Replace with your API key
+    style: 'https://static.maptoolkit.net/rapidapi/styles/terrain.json?rapidapi-key=71c70f9754msha111b1dff3c8c49p172391jsn3e41185c11e5',
     center: [13.404954, 52.520008], 
     zoom: 9
 });
